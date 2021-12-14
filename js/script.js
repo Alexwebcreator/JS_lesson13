6bceb3cc771b20db62232d4b6737bfaaf68e6c82
Alexwebcreator <Sanek72Tob@mail.ru>
2021-12-14 20:19:01 +0500

    Добавил задание урока 13

---
+++ js/script.js
@@ -0,0 +1,73 @@
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+let toDoData = [];
+
+const render = function () {
+  todoList.innerHTML = '';
+  todoCompleted.innerHTML = '';
+
+  toDoData.forEach(function(item, index) {
+    const li = document.createElement('li');
+
+    li.classList.add('todo-item');
+
+    li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+    '<div class="todo-buttons">' +
+		'<button class="todo-remove"></button>' +
+		'<button class="todo-complete"></button>' +
+		'</div>';
+
+    if(item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
+
+    li.querySelector('.todo-complete').addEventListener('click', function() {
+      item.completed = !item.completed;
+      render();
+    });
+
+    li.querySelector('.todo-remove').addEventListener('click', function() {
+      toDoData.splice(index, 1);
+      render();
+    });
+
+    localStorage.setItem("toDoList", JSON.stringify(toDoData));
+    
+  });
+
+  if (toDoData.length == 0) {
+    localStorage.clear();
+  }
+
+};
+
+if (localStorage.toDoList) {
+  toDoData = JSON.parse(localStorage.toDoList);
+  render();
+}
+
+todoControl.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const newToDo = {
+    text: headerInput.value,
+    completed: false
+  };
+
+  if (headerInput.value.trim().length === 0) {
+    alert('Введите текст');
+  } else {
+    toDoData.push(newToDo);
+    headerInput.value = '';
+  }
+
+  render();
+});
+
+
+console.log(localStorage.toDoList);
